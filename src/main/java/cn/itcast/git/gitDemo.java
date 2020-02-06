ec187d7653fd3b2c0b1d6f83db9f264a01dd9bf8
hongte <null_java@163.com>
2020-02-06 10:15:47 +0800

    update src/main/java/cn/itcast/git/gitDemo.java.

--- src/main/java/cn/itcast/git/gitDemo.java
+++ src/main/java/cn/itcast/git/gitDemo.java
@@ -4,5 +4,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第一次增加代码");
+        System.out.println("git上增加代码")                                                                
     }
 }
